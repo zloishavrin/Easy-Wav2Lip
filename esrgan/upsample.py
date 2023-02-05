db1a5ee03fb63dcfd89efb027f4b08afbe5ef3eb
GucciFlipFlops1917 <88063527+GucciFlipFlops1917@users.noreply.github.com>
2023-02-05 15:36:23 -0600

    Add files via upload

---
+++ esrgan/upsample.py
@@ -0,0 +1,53 @@
+import cv2
+import glob
+import os
+import sys
+from basicsr.archs.rrdbnet_arch import RRDBNet
+from basicsr.utils.download_util import load_file_from_url
+import numpy as np
+import torch
+
+from realesrgan import RealESRGANer
+from realesrgan.archs.srvgg_arch import SRVGGNetCompact
+
+def load_sr():
+    model_name = 'RealESRGAN_x4plus'
+    file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth']
+    model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
+    netscale = 4
+
+    model_path = os.path.join('weights', model_name + '.pth')
+    if not os.path.isfile(model_path):
+        ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+        for url in file_url:
+            # model_path will be updated
+            model_path = load_file_from_url(
+                url=url, model_dir=os.path.join(ROOT_DIR, 'weights'), progress=True, file_name=None)
+
+    upsampler = RealESRGANer(
+        scale=netscale,
+        model_path=model_path,
+        dni_weight=None,
+        model=model,
+        tile=0,
+        tile_pad=10,
+        pre_pad=0,
+        half=True,
+        gpu_id=0)
+    return upsampler
+
+
+def upscale(image, upsampler):
+
+    img = image.astype(np.float32) / 255.
+    #img = torch.from_numpy(np.transpose(img[:, :, [2, 1, 0]], (2, 0, 1))).float()
+    #img = img.unsqueeze(0).cuda()
+
+    try:
+        _, _, output = face_enhancer.enhance(img, has_aligned=False, only_center_face=False, paste_back=True)
+        #output, _ = upsampler.enhance(image, outscale=4)
+    except RuntimeError as error:
+        print('Error', error)
+        print('If you encounter CUDA out of memory, try to set --tile with a smaller number.')
+
+    return output
