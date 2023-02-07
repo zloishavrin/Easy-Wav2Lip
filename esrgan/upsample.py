b890f4562288ad9b4b0cbaecf338cc5b2d566268
GucciFlipFlops1917 <88063527+GucciFlipFlops1917@users.noreply.github.com>
2023-02-06 18:33:46 -0600

    Update upsample.py

--- esrgan/upsample.py
+++ esrgan/upsample.py
@@ -9,6 +9,9 @@ import torch
 from gfpgan import GFPGANer
 from realesrgan import RealESRGANer
 from realesrgan.archs.srvgg_arch import SRVGGNetCompact
+from basicsr.utils import imwrite, img2tensor, tensor2img
+from torchvision.transforms.functional import normalize
+from basicsr.utils.registry import ARCH_REGISTRY
 
 def load_sr(model_path):
     #model_name = 'RealESRGAN_x4plus'
@@ -37,19 +40,43 @@ def load_sr(model_path):
     
     face_enhancer = GFPGANer(
         model_path='https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth',
-        upscale=4,
+        upscale=2,
         arch='clean',
         channel_multiplier=2,
         bg_upsampler=upsampler)
-    face_enhancer = 0
-    return upsampler, face_enhancer
+    
+    net = ARCH_REGISTRY.get('CodeFormer')(dim_embd=512, codebook_size=1024, n_head=8, n_layers=9,
+                                          connect_list=['32', '64', '128', '256']).to(device)
+    ckpt_path = load_file_from_url(url='https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/codeformer.pth',
+                                   model_dir='weights/CodeFormer', progress=True, file_name=None)
+    checkpoint = torch.load(ckpt_path)['params_ema']
+    net.load_state_dict(checkpoint)
+    net.eval()
+    return upsampler, face_enhancer, net
 
 
 def upscale(image, face, properties):
     try:
-        if face==True:
+        if face==1:             ## GFP-GAN
             _, _, output = properties.enhance(image, has_aligned=False, only_center_face=False, paste_back=True)
-        else:
+        elif face==2:           ## CODEFORMER
+            net = properties[0]
+            device = properties[1]
+            image = cv2.resize(image, (512, 512), interpolation=cv2.INTER_LINEAR)
+            cropped_face_t = img2tensor(image / 255., bgr2rgb=True, float32=True)
+            normalize(cropped_face_t, (0.5, 0.5, 0.5), (0.5, 0.5, 0.5), inplace=True)
+            cropped_face_t = cropped_face_t.unsqueeze(0).to(device)
+            try:
+                with torch.no_grad():
+                    cropped_face_t = net(cropped_face_t, w=1, adain=True)[0]
+                    restored_face = tensor2img(cropped_face_t, rgb2bgr=True, min_max=(-1, 1))
+                del cropped_face_t
+                torch.cuda.empty_cache()
+            except Exception as error:
+                print(f'\tFailed inference for CodeFormer: {error}')
+                restored_face = tensor2img(cropped_face_t, rgb2bgr=True, min_max=(-1, 1))
+            output = restored_face.astype('uint8')
+        elif face==0:           ## ESRGAN
             img = image.astype(np.float32) / 255.
             output, _ = properties.enhance(image, outscale=4)
     except RuntimeError as error:
