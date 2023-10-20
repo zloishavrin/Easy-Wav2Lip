1a46e5963f2cf8aab376a4e55081637dfab16688
anothermartz <89461919+anothermartz@users.noreply.github.com>
2023-10-20 10:51:08 +0100

    Update easy_functions.py

--- easy_functions.py
+++ easy_functions.py
@@ -104,7 +104,7 @@ def is_url(string):
 
 def load_predictor():
   url = 'https://drive.google.com/uc?id=10dv2sddYJwjdjBdMViYIWr5-R8mRasgu'
-  output = os.path.join(checkpoints,'shape_predictor_68_face_landmarks_GTX.dat')
+  output = os.path.join('checkpoints','shape_predictor_68_face_landmarks_GTX.dat')
   gdown.download(url, output, quiet=False)
 
   predictor = dlib.shape_predictor(output)
