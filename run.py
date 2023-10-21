9f310c6dd92501abac944b90ac341fe6eb338d8d
anothermartz <89461919+anothermartz@users.noreply.github.com>
2023-10-21 23:30:03 +0100

    set preview_input false if not running on colab

--- run.py
+++ run.py
@@ -4,7 +4,8 @@ import re
 from easy_functions import (format_time,
                             get_input_length,
                             get_video_details,
-                            show_video)
+                            show_video,
+                            g_colab)
 import contextlib
 import shutil
 import subprocess
@@ -34,7 +35,10 @@ debug_mask = config.getboolean('MASK', 'debug_mask')
 batch_process = config.getboolean('OTHER', 'batch_process')
 output_suffix = config['OTHER']['output_suffix']
 include_settings_in_suffix = config.getboolean('OTHER', 'include_settings_in_suffix')
-preview_input = config.getboolean('OTHER', 'preview_input')
+if g_colab():
+	preview_input = config.getboolean('OTHER', 'preview_input')
+else:
+	preview_input = False
 preview_settings = config.getboolean('OTHER', 'preview_settings')
 frame_to_preview = config.getint('OTHER', 'frame_to_preview')
 
