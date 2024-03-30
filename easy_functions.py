8ee3908d0b29cdd123e2d8389177e53c38b1c692
anothermartz <89461919+anothermartz@users.noreply.github.com>
2024-03-30 22:02:54 +0000

    Revert "Add CPU support & clean up some code" (#44)
    This reverts commit 777fab359abb3b67bbc52f9be44149f628a46338.

--- easy_functions.py
+++ easy_functions.py
@@ -11,59 +11,39 @@ from base64 import b64encode
 from urllib.parse import urlparse
 from torch.hub import download_url_to_file, get_dir
 from IPython.display import HTML, display
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-
+device = 'cuda'
 
 def get_video_details(filename):
-    cmd = [
-        "ffprobe",
-        "-v",
-        "error",
-        "-show_format",
-        "-show_streams",
-        "-of",
-        "json",
-        filename,
-    ]
-    result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    info = json.loads(result.stdout)
+  cmd = ['ffprobe', '-v', 'error', '-show_format', '-show_streams', '-of', 'json', filename]
+  result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+  info = json.loads(result.stdout)
 
-    # Get video stream
-    video_stream = next(
-        stream for stream in info["streams"] if stream["codec_type"] == "video"
-    )
+  # Get video stream
+  video_stream = next(stream for stream in info['streams'] if stream['codec_type'] == 'video')
 
-    # Get resolution
-    width = int(video_stream["width"])
-    height = int(video_stream["height"])
-    resolution = width * height
+  # Get resolution
+  width = int(video_stream['width'])
+  height = int(video_stream['height'])
+  resolution = width*height
 
-    # Get fps
-    fps = eval(video_stream["avg_frame_rate"])
+  # Get fps
+  fps = eval(video_stream['avg_frame_rate'])
 
-    # Get length
-    length = float(info["format"]["duration"])
+  # Get length
+  length = float(info['format']['duration'])
 
-    return width, height, fps, length
-
+  return width, height, fps, length
 
 def show_video(file_path):
-    """Function to display video in Colab"""
-    mp4 = open(file_path, "rb").read()
-    data_url = "data:video/mp4;base64," + b64encode(mp4).decode()
-    width, _, _, _ = get_video_details(file_path)
-    display(
-        HTML(
-            """
+  """Function to display video in Colab"""
+  mp4 = open(file_path,'rb').read()
+  data_url = "data:video/mp4;base64," + b64encode(mp4).decode()
+  width, _, _, _ = get_video_details(file_path)
+  display(HTML("""
   <video controls width=%d>
       <source src="%s" type="video/mp4">
   </video>
-  """
-            % (min(width, 1280), data_url)
-        )
-    )
-
+  """ % (min(width, 1280), data_url)))
 
 def format_time(seconds):
     hours = int(seconds // 3600)
@@ -71,31 +51,28 @@ def format_time(seconds):
     seconds = int(seconds % 60)
 
     if hours > 0:
-        return f"{hours}h {minutes}m {seconds}s"
+        return f'{hours}h {minutes}m {seconds}s'
     elif minutes > 0:
-        return f"{minutes}m {seconds}s"
+        return f'{minutes}m {seconds}s'
     else:
-        return f"{seconds}s"
-
+        return f'{seconds}s'
 
 def _load(checkpoint_path):
-    if device == "cuda":
+    if device == 'cuda':
         checkpoint = torch.load(checkpoint_path)
     else:
-        checkpoint = torch.load(
-            checkpoint_path, map_location=lambda storage, loc: storage
-        )
+        checkpoint = torch.load(checkpoint_path,
+                                map_location=lambda storage, loc: storage)
     return checkpoint
-
 
 def load_model(path):
     # If results file exists, load it and return
     working_directory = os.getcwd()
     folder, filename_with_extension = os.path.split(path)
     filename, file_type = os.path.splitext(filename_with_extension)
-    results_file = os.path.join(folder, filename + ".pk1")
+    results_file = os.path.join(folder,filename+'.pk1')
     if os.path.exists(results_file):
-        with open(results_file, "rb") as f:
+        with open(results_file, 'rb') as f:
             return pickle.load(f)
     model = Wav2Lip()
     print("Loading {}".format(path))
@@ -103,57 +80,42 @@ def load_model(path):
     s = checkpoint["state_dict"]
     new_s = {}
     for k, v in s.items():
-        new_s[k.replace("module.", "")] = v
+        new_s[k.replace('module.', '')] = v
     model.load_state_dict(new_s)
 
     model = model.to(device)
-    # Save results to file
-    with open(results_file, "wb") as f:
+        # Save results to file
+    with open(results_file, 'wb') as f:
         pickle.dump(model.eval(), f)
-    # os.remove(path)
+    #os.remove(path)
     return model.eval()
-
 
 def get_input_length(filename):
     result = subprocess.run(
-        [
-            "ffprobe",
-            "-v",
-            "error",
-            "-show_entries",
-            "format=duration",
-            "-of",
-            "default=noprint_wrappers=1:nokey=1",
-            filename,
-        ],
+        ["ffprobe", "-v", "error", "-show_entries", "format=duration",
+         "-of", "default=noprint_wrappers=1:nokey=1", filename],
         stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-    )
+        stderr=subprocess.STDOUT)
     return float(result.stdout)
 
-
 def is_url(string):
-    url_regex = re.compile(r"^(https?|ftp)://[^\s/$.?#].[^\s]*$")
+    url_regex = re.compile(r'^(https?|ftp)://[^\s/$.?#].[^\s]*$')
     return bool(url_regex.match(string))
 
-
 def load_predictor():
-    checkpoint = os.path.join(
-        "checkpoints", "shape_predictor_68_face_landmarks_GTX.dat"
-    )
-    predictor = dlib.shape_predictor(checkpoint)
-    mouth_detector = dlib.get_frontal_face_detector()
+  checkpoint = os.path.join('checkpoints','shape_predictor_68_face_landmarks_GTX.dat')
+  predictor = dlib.shape_predictor(checkpoint)
+  mouth_detector = dlib.get_frontal_face_detector()
 
-    # Serialize the variables
-    with open(os.path.join("checkpoints", "predictor.pkl"), "wb") as f:
-        pickle.dump(predictor, f)
+  # Serialize the variables
+  with open(os.path.join('checkpoints','predictor.pkl'), 'wb') as f:
+      pickle.dump(predictor, f)
 
-    with open(os.path.join("checkpoints", "mouth_detector.pkl"), "wb") as f:
-        pickle.dump(mouth_detector, f)
+  with open(os.path.join('checkpoints','mouth_detector.pkl'), 'wb') as f:
+      pickle.dump(mouth_detector, f)
 
-    # delete the .dat file as it is no longer needed
-    # os.remove(output)
-
+  #delete the .dat file as it is no longer needed
+  #os.remove(output)
 
 def load_file_from_url(url, model_dir=None, progress=True, file_name=None):
     """Load file form http url, will download models if necessary.
@@ -172,7 +134,7 @@ def load_file_from_url(url, model_dir=None, progress=True, file_name=None):
     """
     if model_dir is None:  # use the pytorch hub_dir
         hub_dir = get_dir()
-        model_dir = os.path.join(hub_dir, "checkpoints")
+        model_dir = os.path.join(hub_dir, 'checkpoints')
 
     os.makedirs(model_dir, exist_ok=True)
 
@@ -186,11 +148,9 @@ def load_file_from_url(url, model_dir=None, progress=True, file_name=None):
         download_url_to_file(url, cached_file, hash_prefix=None, progress=progress)
     return cached_file
 
-
 def g_colab():
     try:
         import google.colab
-
         return True
     except ImportError:
         return False
